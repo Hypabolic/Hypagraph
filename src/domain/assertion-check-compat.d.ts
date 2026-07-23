22c0675602a840e6ab44b829ca31532667c11980
Matthew Gribben <matthew@hypabolic.com>
2026-07-23 17:12:57 +0500

    Keep assertion checks source-compatible with Pi extension

---
+++ src/domain/assertion-check-compat.d.ts
@@ -0,0 +1,15 @@
+declare module "./model.js" {
+  interface FileAssertionCheckDefinition {
+    /** Transitional source compatibility for Pi invocation labels. */
+    command?: undefined;
+    arguments?: undefined;
+  }
+
+  interface GitAssertionCheckDefinition {
+    /** Transitional source compatibility for Pi invocation labels. */
+    command?: undefined;
+    arguments?: undefined;
+  }
+}
+
+export {};
