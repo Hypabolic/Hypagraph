7be0e1bfeb0c56c4cd1e3788636c9d5a9d9da9c8
Matthew Gribben <matthew@hypabolic.com>
2026-07-23 17:21:56 +0500

    Anchor assertion compatibility augmentation

--- src/domain/assertion-check-compat.d.ts
+++ src/domain/assertion-check-compat.d.ts
@@ -1,3 +1,5 @@
+import "./model.js";
+
 declare module "./model.js" {
   interface FileAssertionCheckDefinition {
     /** Transitional source compatibility for Pi invocation labels. */
@@ -11,5 +13,3 @@ declare module "./model.js" {
     arguments?: undefined;
   }
 }
-
-export {};
